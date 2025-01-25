848e6d627d94867266a7d1a5456aab22a4acc473
achibhossengit <mail.achibhossen@gmail.com>
2025-01-25 13:45:43 +0600

    create common dashboard

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # from .views import show_task
-from tasks.views import manager_dashboard, user_dashboard
+from tasks.views import manager_dashboard, user_dashboard, dashboard
 urlpatterns = [
     path('manager-dashboard', manager_dashboard),
-    path('user-dashboard', user_dashboard)
+    path('user-dashboard', user_dashboard),
+    path('common-dashboard', dashboard)
 ]
