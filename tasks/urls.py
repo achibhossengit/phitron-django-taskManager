fb19ec42ec8d43c6ef9341b8caa759349719aed7
achibhossengit <mail.achibhossen@gmail.com>
2025-01-25 12:25:41 +0600

    remove all test file

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 # from .views import show_task
-from tasks.views import show_task, show_specific_task, dashboard_rendaring
+from tasks.views import dashboard_rendaring
 urlpatterns = [
-    path('show-task', show_task),
-    # dynamic urls
-    path('show-task/<id>/', show_specific_task),
     path('dashboard', dashboard_rendaring)
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,18 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request): #here home is a view
-    return HttpResponse("Wellcome to task management system!")
-
-def contact(request):
-    return HttpResponse("<h1 style='color: green; background-color:red'>This is contact page of mine website!<h1>")
-
-def show_task(request):
-    return HttpResponse("This page is all about show task.")
-
-def show_specific_task(request, id):
-    print(type(id))
-    return HttpResponse(f"This is task: {id}")
-
 def dashboard_rendaring(request):
     return render(request, "dashboard.html")
