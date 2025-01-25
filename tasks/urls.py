de7397bb6e2868b59743a0bef69cf4715428f51c
achibhossengit <mail.achibhossen@gmail.com>
2025-01-25 12:03:43 +0600

    again dashboard in main brach

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from .views import show_task
-from tasks.views import show_task, show_specific_task
+from tasks.views import show_task, show_specific_task, dashboard_rendaring
 urlpatterns = [
     path('show-task', show_task),
     # dynamic urls
-    path('show-task/<id>/', show_specific_task)
+    path('show-task/<id>/', show_specific_task),
+    path('dashboard', dashboard_rendaring)
 ]
