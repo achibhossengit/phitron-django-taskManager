c15e9f12569faf6b8c6650e064fec0c61a646833
achibhossengit <mail.achibhossen@gmail.com>
2025-02-05 17:48:43 +0600

    task creations with django form

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from tasks.forms import TaskForm
-from tasks.models import  Employee
+from tasks.models import  Task, Employee
 
 # Create your views here.
 def dashboard(request):
@@ -24,5 +24,19 @@ def create_task(request):
     employees = Employee.objects.all()
     # print(employees)
     form = TaskForm(employees = employees)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, employees = employees)
+        if form.is_valid():
+            data = form.cleaned_data # here, data is a dictionary
+            title = data.get('title') # get: to access dictionary data, if dictionary have't title key, its return none
+            description = data.get('description')
+            due_date = data.get('due_date')
+            assigned_to = data.get('assigned_to') # here, is a many to many relations
+
+            task = Task.objects.create(title=title, description = description, due_date=due_date)
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id = emp_id)
+                task.assigned_to.add(employee)
+
     context = {'form':form}
     return render(request, 'create_task.html', context)
