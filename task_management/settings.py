34c20d8808f8045c1cc6c97600f88a127c63ba47
achibhossengit <mail.achibhossen@gmail.com>
2025-02-20 12:27:40 +0600

    implement email send features using signals for manay to manay field

--- task_management/settings.py
+++ task_management/settings.py
@@ -151,3 +151,11 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mail.achibhossen@gmail.com'
+EMAIL_HOST_PASSWORD = 'sbqv xqld jfpj tiid'
