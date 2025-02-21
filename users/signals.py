e449749b8c1c4dd226a9a1a8dbc540f40c447f72
achibhossengit <mail.achibhossen@gmail.com>
2025-02-21 10:06:11 +0600

    use djanog authentication form for log in

---
+++ users/signals.py
@@ -0,0 +1,20 @@
+# confirmation email
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+@receiver(post_save, sender=User)
+def confirmation_email_sent(sender, instance, created, **kwargs):
+    if created:
+        token = default_token_generator.make_token(instance)
+        activation_url = f"http://127.0.0.1:8000/user/{instance.id}/{token}"
+        recipient_list = [instance.email]
+        subject = 'Activate your account'
+        message = f"Hello {instance.username}, \n\nPlease click the link bellow to activate your employee your account.\n Activation Link: {activation_url}"
+        try:
+            send_mail(subject, message, settings.EMAIL_HOST_USER, recipient_list)
+        except Exception as e:
+            print(f'Sending email failed. Employee: {instance} \n Error is: {str(e)}')
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from users.forms import RegisterForm, CustomRegisterForm
@@ -9,38 +9,31 @@ from django.contrib import messages
 def sign_up(request):
     if request.method == 'GET':
         form = CustomRegisterForm()
+        return render(request, 'register/sign_up.html', {'form': form})
     elif request.method == 'POST':
         form = CustomRegisterForm(request.POST)
         if form.is_valid():
-            # username = form.cleaned_data.get('username')
-            # password = form.cleaned_data.get('password1')
-            # confirm_password = form.cleaned_data.get('password2')
-            # if password == confirm_password:
-            #     User.objects.create(username= username, password=password)
-            # else:
-            #     print("Password aren't same!")
-        # else:
-        #     print("form is invalid!")
+            user = form.save(commit=False)
+            print('user:', user)
+            user.set_password(form.cleaned_data.get('password')) # for hasing
+            user.is_active = False
+            user.save()
+            messages.success(request, 'User Added Successfully! Please check your email for log in.')
+            return redirect('sign-in')
+        else:
+            print('Form Validation error')
+            return render(request, 'register/sign_up.html', {'form': form})
 
-            form.save()
-            messages.success(request, 'User Added Successfully!')
-        
-    return render(request, 'register/sign_up.html', {'form': form})
 
 def sign_in(request):
+    form = AuthenticationForm(data = request.POST)
     if request.method=='POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password= password)
-        if user is not None:
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect('home')
-        else:
-            messages.error(request, "user not found")
-            print('invalid user')
-            return redirect('sign-in')
 
-    return render(request, 'register/sign_in.html') # for get request
+    return render(request, 'register/sign_in.html', {'form': form}) # for get request
 
 
 def sign_out(request):
