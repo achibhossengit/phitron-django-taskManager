d7d0c6fd05629f3238335844993c51b510962bd2
achibhossengit <mail.achibhossen@gmail.com>
2025-02-25 21:14:43 +0600

    optimized qroups_list query

--- users/views.py
+++ users/views.py
@@ -99,5 +99,6 @@ def create_group(request):
 
 @user_passes_test(is_admin, login_url='no-permission')
 def group_list(request):
-    groups = Group.objects.all()
+    groups = Group.objects.prefetch_related('permissions').all()
+    
     return render(request, 'admin/group_list.html', {'groups': groups})
