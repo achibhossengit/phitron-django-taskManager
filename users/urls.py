6f3d8aa34f5fff081a0752f3a3486a2c330e28c7
achibhossengit <mail.achibhossen@gmail.com>
2025-03-15 23:02:27 +0600

    add extends on profile.html and use LogoutView

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from users.views import sign_up, sign_in, sign_out, active_user, admin_dashboard, manager_dashboard, employee_dashboard, assign_role, create_group, group_list, CustomLoginView, ProfileView
 from core.views import home
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/dashboard/', admin_dashboard, name='admin-dashboard'),
     path('manager-dashboard/', manager_dashboard, name='manager-dashboard'),
@@ -8,7 +9,8 @@ urlpatterns = [
     path('sign-up/', sign_up, name='sign-up'),
     # path('sign-in/', sign_in, name='sign-in'),
     path('sign-in/', CustomLoginView.as_view(template_name="register/sign_in.html"), name='sign-in'),
-    path('sign-out/', sign_out, name='sign-out'),
+    # path('sign-out/', sign_out, name='sign-out'),
+    path('sign-out/', LogoutView.as_view(), name='sign-out'),
     path('', home, name='home'),
     path('activate/<int:user_id>/<str:token>', active_user),
     path('admin/<int:user_id>/assign-role/ ', assign_role, name='assign-role'),
